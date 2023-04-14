57df28d5f96ae0ecf26178d4c96c8a97ce6ea541
argalad <github@argalad.com>
2023-04-14 18:45:57 +0200

    Ejercicios de diccionarios

---
+++ 4. Diccionarios/49 - Me pillo el toro/49.cpp
@@ -0,0 +1,78 @@
+// Nombre y Apellidos: Alberto Rodríguez - Rabadán Manzanares
+// Número de juez: 77
+
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+map<string, int> resolver(int n) {
+    map<string, int> scores;
+    cin.ignore();  // descartar salto de línea
+
+    for (int i = 0; i < n; i++) {
+        string name;
+        string status;
+
+        getline(cin, name);
+        getline(cin, status);
+
+        if (status == "CORRECTO")
+            scores[name]++;
+        else
+            scores[name]--;
+    }
+
+    return scores;
+}
+
+void mostrar_sol(const map<string, int>& scores) {
+    bool first = true;
+
+    for (auto& p : scores) {
+        if (p.second != 0) {
+            if (!first)
+                cout << endl;
+            else
+                first = false;
+            cout << p.first << ", " << p.second;
+        }
+    }
+    cout << endl << "---" << endl;
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    int n;
+    // Leer los datos de la entrada
+    cin >> n;
+    if (n == 0)
+        return false;
+
+    // Resolver problema y mostrar solución
+    auto sol = resolver(n);
+    mostrar_sol(sol);
+
+    return true;
+}
+
+int main() {
+    // Ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+    ifstream in("casos.txt");
+    auto cinbuf = cin.rdbuf(in.rdbuf());
+#endif
+
+    while (resuelveCaso())
+        ;
+
+        // Para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+    cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+    return 0;
+}
---
+++ 4. Diccionarios/52 - Capitulos repetidos/52.cpp
@@ -0,0 +1,56 @@
+// Nombre y Apellidos: Alberto Rodríguez - Rabadán Manzanares
+// Número de juez: 77
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <unordered_map>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+    int n;
+    cin >> n;
+
+    unordered_map<int, int> capitulos;
+    int maxPeriodo = 0, periodoActual = 0;
+
+    for (int i = 0; i < n; i++) {
+        int capitulo;
+        cin >> capitulo;
+
+        if (capitulos.find(capitulo) != capitulos.end())
+            periodoActual = min(i - capitulos[capitulo], periodoActual + 1);
+        else
+            periodoActual++;
+
+        capitulos[capitulo] = i;
+        maxPeriodo = max(maxPeriodo, periodoActual);
+    }
+
+    cout << maxPeriodo << endl;
+}
+
+int main() {
+    // Ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+    ifstream in("casos.txt");
+    auto cinbuf = cin.rdbuf(in.rdbuf());
+#endif
+
+    int numCasos;
+    cin >> numCasos;
+    for (int i = 0; i < numCasos; i++)
+        resuelveCaso();
+
+        // Para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+    cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+    return 0;
+}
